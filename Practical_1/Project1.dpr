5a698bdbb4bc99af9cbc487ddea2462470ecbca7
CakesTwix <oleg.kiryazov@gmail.com>
2020-10-15 14:42:38 +0300

    Practical1: Initial commit

---
+++ Practical_1/Project1.dpr
@@ -0,0 +1,120 @@
+﻿program Window;
+uses Windows, Messages;
+const AppName='API Window';
+var AMessage : TMsg;
+
+//Определяем элементы интерфейса
+
+//Само окно
+hWindow : HWnd;
+
+//Кнопки
+hBtnExit : HWnd;
+hBtnAbout : HWnd;
+
+//Лейблы
+hLabel_1 : HWnd;
+hLabel_2 : HWnd;
+hLabel_3 : HWnd;
+
+//Поле ввода текста
+hEdit : HWnd;
+
+//Функция, которая обрабатывает кнопки
+function WindowProc(Window:HWnd; AMessage, WParam, LParam:longint):longint; stdcall; export;
+begin
+WindowProc:=0;
+case AMessage of
+  WM_DESTROY: begin PostQuitMessage(0); Exit; end;
+  WM_COMMAND:
+
+  //Обработчик кнопки выхода
+  if LParam=hBtnExit then begin PostQuitMessage(0); Exit; end
+
+  //Обработчик кнопки Про окно
+  else if LParam=hBtnAbout then MessageBox(Window,
+    'Вікно, створене функціями API, без використання VCL Delphi.'+#13#13+
+    'Copyright© Microsoft... і т.д.','API Window',MB_OK or MB_ICONINFORMATION);
+  WM_KEYDOWN: if (WParam=VK_RETURN) or (WParam=VK_ESCAPE) then
+    SendMessage(hBtnExit,BM_CLICK,0,0)
+    else if WParam=VK_F1 then SendMessage(hBtnAbout,BM_CLICK,0,0);
+  end;
+
+WindowProc:=DefWindowProc(Window, AMessage, WParam, LParam);
+end;
+
+
+function WinRegister: boolean;
+var WindowClass: TWndClass;
+begin
+with WindowClass do begin
+Style:=CS_HREDRAW or CS_VREDRAW;
+lpfnWndProc:=@WindowProc;
+cbClsExtra:=0;
+cbWndExtra:=0;
+hInstance:=HInstance;
+hIcon:=LoadIcon(0, IDI_APPLICATION);
+hCursor:=LoadCursor(0, IDC_ARROW);
+hbrBackGround:=COLOR_WINDOW;
+lpszMenuName:=nil;
+lpszClassName:=AppName;
+end;
+Result:=RegisterClass(WindowClass)<>0;
+end;
+
+//Функция, которая создает окно и раставляет элементы
+function WinCreate: HWnd;
+  var hWindow : HWnd;
+    begin
+      hWindow:=CreateWindow(AppName, 'Вікно, створене з використанням API',WS_OVERLAPPEDWINDOW,
+      100, 100, 600, 400, 0, 0, HInstance, nil);
+    if hWindow<>0 then
+      begin
+        //Параметры окна
+        ShowWindow(hWindow, SW_SHOWNORMAL);
+        UpdateWindow(hWindow);
+
+        //Параметры кнопки выхода
+        hBtnExit:=CreateWindow('BUTTON', 'Вихід',WS_CHILD or BS_DEFPUSHBUTTON or WS_TABSTOP, 100,
+        10, 90, 30, hWindow, 0, HInstance, nil);
+
+        //Параметры лейблов
+        hLabel_1:=CreateWindow('Static','Это первый статический текст',WS_VISIBLE or WS_CHILD or SS_LEFT, 10,10,360,44,hWindow,0,hInstance,nil);
+        hLabel_2:=CreateWindow('Static','Это второй статический текст',WS_VISIBLE or WS_CHILD or SS_LEFT, 10,30,360,44,hWindow,0,hInstance,nil);
+        hLabel_3:=CreateWindow('Static','Это третий статический текст',WS_VISIBLE or WS_CHILD or SS_LEFT, 10,50,360,44,hWindow,0,hInstance,nil);
+
+        //Параметры поля ввода
+        hEdit:=CreateWindowEx(WS_EX_CLIENTEDGE,'Edit','Введите текст', WS_VISIBLE or WS_CHILD or ES_LEFT or ES_AUTOHSCROLL,10,70,360,23,hWindow,0,hInstance,nil);
+
+
+        if hBtnExit<>0 then ShowWindow(hBtnExit, SW_SHOWNORMAL);
+          SendMessage(hBtnExit,WM_SETFONT, CreateFont(18,0,0,0,700,0,0,0,
+          ANSI_CHARSET,OUT_DEFAULT_PRECIS,CLIP_DEFAULT_PRECIS,DEFAULT_QUALITY,DEFAULT_PITCH,
+          'Times New Roman Cyr'),1);
+
+         //Параметры кнопки Про окно
+         hBtnAbout:=CreateWindow('BUTTON', 'Про вікно...',WS_CHILD or WS_TABSTOP, 500, 75, 90, 30,
+         hWindow, 0, HInstance, nil);
+          if hBtnAbout<>0 then ShowWindow(hBtnAbout, SW_SHOWNORMAL);
+      end;
+    Result:=hWindow;
+    end;
+begin
+  if not WinRegister then
+    begin
+      MessageBox(0,'Клас вікна не зареєстровано',AppName,MB_OK);
+      Exit;
+    end;
+  hWindow:=WinCreate;
+  if hWindow=0 then
+    begin
+      MessageBox(0,'Не вийшло створити вікно.',AppName,MB_OK);
+      Exit;
+    end;
+  while GetMessage(AMessage,0,0,0) do
+    begin
+      TranslateMessage(AMessage);
+      DispatchMessage(AMessage);
+    end;
+  Halt(AMessage.wParam);
+end.
